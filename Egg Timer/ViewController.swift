47d56f8ce5a3891a15c2823506d5a9fea7072052
Jay Bhensdadia <jayvpatel29@gmail.com>
2022-10-28 21:49:19 +0530

    Setting up timer

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    let eggTimes = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
+    let eggTimes = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
+    var secondsRemaining = 60
     
 
     override func viewDidLoad() {
@@ -25,8 +26,17 @@ class ViewController: UIViewController {
         let hardness = sender.currentTitle!
     
         let result = eggTimes[hardness]!
-        print(result)
+        secondsRemaining = result
         
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer(){
+        if secondsRemaining > 0{
+            print("\(secondsRemaining) seconds")
+            secondsRemaining -= 1
+        }
     }
     
 
