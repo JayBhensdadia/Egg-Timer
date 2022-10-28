dcdd1a5347d2438280d66243d74905102d58129b
Jay Bhensdadia <jayvpatel29@gmail.com>
2022-10-28 21:43:33 +0530

    using dictionary in place of if else statement

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -10,10 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
-    
+    let eggTimes = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
     
 
     override func viewDidLoad() {
@@ -25,18 +22,11 @@ class ViewController: UIViewController {
 
     
     @IBAction func HardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
+    
+        let result = eggTimes[hardness]!
+        print(result)
         
-        switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            print("some error")
-        }
     }
     
 
