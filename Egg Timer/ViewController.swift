621757af6653968cbb15406e83e784025425a856
Jay Bhensdadia <jayvpatel29@gmail.com>
2022-10-28 21:10:24 +0530

    setting up UI

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func HardnessSelected(_ sender: UIButton) {
+        print(sender.currentTitle)
+    }
+    
 
 }
 
