df16ce5f9ff418c020ee20e476c6e86d26326f7e
Jay Bhensdadia <jayvpatel29@gmail.com>
2022-10-28 21:20:14 +0530

    egg timer with if statements

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -8,15 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
     
     @IBAction func HardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle)
+        let hardness = sender.currentTitle
+        
+        if hardness == "Soft"{
+            print(softTime)
+        }else if hardness == "Medium"{
+            print(mediumTime)
+        }else{
+            print(hardTime)
+        }
     }
     
 
